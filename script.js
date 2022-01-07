90dbbed774408d1ba3e8a8b1b41d9280a1ec91b0
LucassSN <lucassouzaemp@gmail.com>
2022-01-07 11:29:27 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+const btn1 = document.getElementById("btn1")
+const textArea = document.getElementById("text_area")
+
+
+
+btn1.addEventListener("click",function(){
+    alert("Hello word!")
+    
+
+})
