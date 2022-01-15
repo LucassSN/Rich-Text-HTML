2ff7380e60541d5bc77ba7754f1d3049ab5e78a8
LucassSN <lucassouzaemp@gmail.com>
2022-01-15 14:47:16 -0300

    last commit

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 const btns = document.querySelectorAll('.btn')
 
 btns.forEach(btns => {
-    btns.addEventListener('click',()=>{
+    btns.addEventListener('click', () => {
         let command = btns.dataset['element'];
-        if(command == 'createLink'){
+        if (command == 'createLink') {
             let url = prompt('Insert Link:', 'https://')
-        document.execCommand(command,false,url);
-    }else{
-        if(command == "insertImage"){
-            let url = (prompt('Insert Link:'))
-            document.execCommand(command,false,url);
+            document.execCommand(command, false, url);
+        } else {
+            if (command == "insertImage") {
+                let url = (prompt('Insert Link:'))
+                document.execCommand(command, false, url);
 
-        }else{
-            document.execCommand(command,false,null)
+            } else {
+                document.execCommand(command, false, null)
+            }
         }
-    }
     })
 
 })
